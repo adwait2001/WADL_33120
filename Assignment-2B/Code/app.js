16aab5a165709a11620e35c098d1b4aeaf7a854d
adwait2001 <adwaitnsk.2009@rediffmail.com>
2022-04-30 02:09:41 +0530

    first commit

---
+++ Assignment-2B/Code/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3030;
+
+
+app.get("/", (req, res) =>{
+    res.send("Hello World! 33120 has started Docker!!!!!");
+});
+
+app.listen(port, (err) => {
+    if (err) {
+      console.log('Error::', err);
+    }
+    console.log(`Onexlab app listening on port ${port}`);
+});
